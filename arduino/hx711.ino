f7544fdf155e746f56decbd5e890c90fcd829f73
sammy <sammyoina@gmail.com>
2021-06-19 17:51:06 +0300

    Update hx711.ino

--- arduino/hx711.ino
+++ arduino/hx711.ino
@@ -7,16 +7,23 @@ HX711 scale;
 //TO DO
 //set reference and offseet
 void setup() {
+   float unit = -4452.50;
+   float reference_i = 210.0;
+   float deviation = 3.40;
   Serial.begin(9600);
   //Serial.println("start");
   scale.begin(DT_PIN, SCK_PIN);
+  Serial.println(scale.get_units(10));
 
   scale.set_scale(unit/reference_i); //set reference units
+  Serial.println(scale.get_units(10));
   scale.tare();
+  Serial.println(scale.get_units(10));
 
-  scale.set_offset(deviation); //definition of offset?
+  //scale.set_offset(deviation); //definition of offset?
 
   Serial.println(scale.get_units(10));
+  Serial.println("here");
 }
 
 
